0f601f6faf23ce71b9397326cbe57263499ef1e4
danorel <mail.ordan@gmail.com>
2021-03-27 17:52:05 +0200

    [Client + Server]: Created the Typescript core for backend. Integrated the React-app structure with tsx.

---
+++ server/src/app.ts
@@ -0,0 +1,40 @@
+import express, { Application, Request, Response, NextFunction } from 'express';
+import createError, { HttpError } from 'http-errors';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+
+const app: Application = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// catch 404 and forward to error handler
+// @ts-ignore
+app.use(function (req: Request, res: Response, next: NextFunction) {
+    next(createError(404));
+});
+
+// error handler
+// @ts-ignore
+app.use((err: HttpError, req: Response, res: Response, next: NextFunction) => {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+    next();
+});
+
+app.listen(3000, () => console.log("Listening to the server onn port 3000."))
+
+module.exports = app;
